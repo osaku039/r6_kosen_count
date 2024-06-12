139a9cbd72bc9a959d320e1d61508a5636f8a577
bota_moti <kubotagakuto@gmail.com>
2024-06-12 09:36:57 +0900

    コメント.ver1.0

--- R6_Kosen_Count/thank.php
+++ R6_Kosen_Count/thank.php
@@ -1,10 +1,9 @@
 //ここにカラム保存の仕組みを作る
-//　できてる？
 <html>
     <head>
         <body>
             <?php echo "投票ありがとうございます。"; ?>
-            <meta http-equiv="refresh" content=" 5; url=http://localhost/R6_Kosen_Count/count_ClassPoint.php">
+            <meta http-equiv="refresh" content=" 5; url=http://localhost/R6_Kosen_Count/count_ClassPoint.php">  //5秒待ってからcount_ClassPint,phpへ移動
         </body>
     </head>
 </html>
