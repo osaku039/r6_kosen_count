fcb6ec9761753fe9605d104e3f1c548080cf4760
bota_moti <kubotagakuto@gmail.com>
2024-06-12 09:33:57 +0900

    コメント.ver.1.0

--- R6_Kosen_Count/index.php
+++ R6_Kosen_Count/index.php
@@ -1,6 +1,6 @@
 <?php 
-require("./dbconnect.php");
-session_start();
+require("./dbconnect.php");  //DB接続
+session_start();  //セッション開始
 
 if (!empty($_POST)) {
     /* 入力情報の不備を検知 */
@@ -15,7 +15,7 @@ if (!empty($_POST)) {
     if (!isset($error)) {
         $_SESSION['join'] = $_POST;   // フォームの内容をセッションで保存
         header('Location: count_ClassPointo.php');   // check.phpへ移動
-        exit();
+        exit(); //セッションを閉じる
     }
 }
 ?>
@@ -29,15 +29,15 @@ if (!empty($_POST)) {
     <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
-    <div class="content">
-        <form action="" method="POST">
+    <div class="content"> <!--divクラスがcontent-->
+        <form action="" method="POST"> <!--formアクション-->
             <h1>クラス杯投票システム</h1>
             <p>クラス名を入力してください(半角入力のみ)</p>
             <br>
-            <div class="control">
+            <div class="control"> <!--divクラスがcontrol-->
                 <label for="class_year">学年　例:3年であれば3のみを入力してください<span class="required">必須</span></label>
                 <input type="text" name="class_year">
-                <?php if (!empty($error["class_year"]) && $error['class_year'] === 'blank'): ?>
+                <?php if (!empty($error["class_year"]) && $error['class_year'] === 'blank'): ?>  //入力されていなかった場合
                     <p class="error">＊クラス名を入力してください。</p>
                 <?php endif ?>
             </div>
@@ -45,14 +45,14 @@ if (!empty($_POST)) {
             <div class="control">
                 <label for="class_number">クラス　例：1組の場合、「1」　メディアであれば「mi」と入力してください<span class="required">必須</span></label>
                 <input type="text" name="class_number">
-                <?php if (!empty($error["class_number"]) && $error['class_number'] === 'blank'): ?>
-                    <p class="error">＊パスワードを入力してください</p>
+                <?php if (!empty($error["class_number"]) && $error['class_number'] === 'blank'): ?> //入力されていなかった場合
+                    <p class="error">＊クラス名を入力してください</p>
                 <?php endif ?>
             </div>
 
 
-            <div class="control">
-                <button type="submit" class="btn">確認する</button>
+            <div class="control"> <!--divクラスがcontrol-->
+                <button type="submit" class="btn">確認する</button>  <!--ボタンを押すことによって送信-->
             </div>
         </form>
     </div>
