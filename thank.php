e9435cc760d10ca9413c6507831aa7cc7ba97c61
bota_moti <kubotagakuto@gmail.com>
2024-06-18 15:39:12 +0900

    コメント.ver1.1

--- thank.php
+++ thank.php
@@ -1,12 +1,13 @@
-//ここにカラム保存の仕組みを作る
+<!-- //ここにカラム保存の仕組みを作る -->
 <?php
-//セッションじゃなくてJSの言語を使い、評価した値をこのthank.php内でカラム保存を作りたい。
+// セッションじゃなくてJSの言語を使い、評価した値をこのthank.php内でカラム保存を作りたい。
 ?>
 <html>
     <head>
         <body>
             <?php echo "投票ありがとうございます。"; ?>
-            <meta http-equiv="refresh" content=" 5; url=http://localhost/R6_Kosen_Count/count_ClassPoint.php">  //5秒待ってからcount_ClassPint,phpへ移動
+            <meta http-equiv="refresh" content=" 5; url=http://localhost/R6_Kosen_Count/count_ClassPoint.php">  
+            <!-- //5秒待ってからcount_ClassPint,phpへ移動 -->
         </body>
     </head>
 </html>
