a414a343797bff458b5e38689e79a303e295a63e
bota-moti <159561146+bota-moti@users.noreply.github.com>
2024-06-18 15:22:53 +0900

    Delete R6_Kosen_Count directory

--- R6_Kosen_Count/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php //DB接続をするためのファイル
-try {
-    $db = new PDO('mysql:dbname=count_classpoint;host=localhost:3305', 'root', '');  //PDOによるDB接続
-}   catch (PDOException $e) {
-    echo "データベース接続エラー　：".$e->getMessage();
-}
-?>
