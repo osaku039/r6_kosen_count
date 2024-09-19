43f978398ce0a8e21fa29b115019bbdd4899d2d9
bota_moti <kubotagakuto@gmail.com>
2024-09-19 11:34:29 +0900

    buttonの追加

---
+++ index_home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <title>グラス杯投票システム</title>
+    <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/style_index.css">
+</head>
+<body>
+    <div class="content_home"> <!--divクラスがcontent-->
+        <form action="" method="POST"> <!--formアクション-->
+            <h1>クラス投票システム</h1>
+            <dic class="class_table">
+                <table>
+                    <tr>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                    </tr>
+                </table> 
+                <br>
+                <table>
+                    <tr>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                    </tr>
+                </table> 
+                <br>
+                <table>
+                    <tr>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                    </tr>
+                </table> 
+                <br>
+                <table>
+                    <tr>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                    </tr>
+                </table> 
+                <br>
+                <table>
+                    <tr>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                        <td><button type="submit" class="btn">確認する</button></td>
+                    </tr>
+                </table> 
+            </div>     
+        </form> 
+    </div>
+</body>
+</html>
